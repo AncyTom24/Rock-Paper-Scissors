569d89b3996bdfca4bd7b7667ad8212bb6d627bb
Ancy Tom <ancytom24@gmail.com>
2021-04-28 21:57:15 -0400

    Add project files.

---
+++ Rock Paper Scissors/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication19;
+using Rock_Paper_Scissors;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game games = new Game();
+            games.Start();
+        }
+    }
+}
